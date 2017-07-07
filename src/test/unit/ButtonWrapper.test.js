23997e626491f386600d9d433bdfabad8a04aa8d
Joe Heitkamp <noetic97@gmail.com>
2017-07-07 17:24:51 -0600

    WIP add more tests

--- src/test/unit/ButtonWrapper.test.js
+++ src/test/unit/ButtonWrapper.test.js
@@ -3,11 +3,26 @@ import { shallow, mount } from 'enzyme';
 import ButtonWrapper from '../../components/ButtonWrapper';
 
 describe('ButtonWrapper.js', () => {
-  it('should render three buttons when it mounts', () => {
+  it('should render a section', () => {
+    const wrapper = shallow(<ButtonWrapper />);
+
+    expect(wrapper.find('section').length).toEqual(1);
+  });
+
+  it('should render three buttons when it renders', () => {
+    const wrapper = shallow(<ButtonWrapper />);
+
+    expect(wrapper.find('Button').length).not.toBe(2);
+    expect(wrapper.find('Button').length).toBe(3);
+  });
+
+  it('should render three buttons with proper titles when it renders', () => {
     const wrapper = mount(<ButtonWrapper />);
 
-    console.log(wrapper.debug());
-
-    expect(wrapper.find('button').length).toEqual(3);
+    expect(wrapper.containsAnyMatchingElements([
+      <button>people</button>,
+      <button>planets</button>,
+      <button>vehicles</button>,
+    ])).toEqual(true)
   });
 });
